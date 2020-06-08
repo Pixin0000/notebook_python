e22031eda5b81564717830ed5deebe91529b582a
pixin0000 <simon121705@gmail.com>
2020-06-08 19:31:56 +0800

    final

--- opencv/opencv_final.py
+++ opencv/opencv_final.py
@@ -1,58 +1,16 @@
 import cv2
-import numpy as np 
-def main():
-    input_img = cv2.imread("images/car.jpg",-1)
-    #讀入汽車圖片
-    trans_img = transform(input_img)
-    #取出車牌的部份圖形
-    gray_img = cvt_gray(trans_img)
-    #轉換為灰階去二值化
-    grad_img = Sobel_gradient(gray_img,3)
-    #使用梯度讓車牌輪廓更明顯
-    black_white_img = devided(gray_img)
-    #二值化
-    blur_img = blur(black_white_img)
-    #濾波器濾掉雜質
-    nohole_img = hole_filling(blur_img)
-    #去除白色破洞
-    canny_img = cv2.Canny(nohole_img,30,100)
-    #找出線條
-    himage = getHProjection(canny_img)
-    #切出文字
-    yimage = getVProjection(himage)
-    #分割文字
-    '''
-    cv2.imshow("car_origin",input_img)
-    #show 原始圖 
-    cv2.imshow("after_trans",trans_img)
-    #show 翻轉圖
-    cv2.imshow("gray",gray_img)
-    cv2.imshow("grad", grad_img)
-    # show 梯度圖
-    '''
-    cv2.imshow("blur",blur_img)
-    # show 濾波圖
-    cv2.imshow("nohole",nohole_img)
-    #show補漏洞圖
-    cv2.imshow("black",black_white_img)
-    #show二值化後的圖
-    cv2.imshow("canny",canny_img)
-    #show線條圖
-    cv2.imshow("himage",himage)
-    #移除圖片雜質
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+import numpy as np
 def transform(input_img):#轉為正面
     x1,y1 = 212,320
     x2,y2 = 220,465
     x3,y3 = 545,485
     x4,y4 = 560,330
+    #取得車牌的4點
     pts1 = np.float32([[x1,y1],[x2,y2],[x3,y3],[x4,y4]])
     #車牌區間
     pts2 = np.float32([[0,0],[0,500],[650,500],[650,0]])
-    #視窗尺寸
+    #設定轉置後的座標
     T = cv2.getPerspectiveTransform(pts1,pts2)
-    #由1轉換為2
     trans_img = cv2.warpPerspective(input_img,T,(650,500))
     return trans_img
 def cvt_gray(trans_img):
@@ -61,10 +19,12 @@ def cvt_gray(trans_img):
     return gray_img
 def blur(black_white_img):
     names = locals()
+    #定義local變數
     names["img%s" %0] = black_white_img
+    #定義第一張圖
     for i in range(1,100):
         names["img%s" %i] = cv2.medianBlur(names["img%s" %(i-1)],3)
-    print(names)
+        #看要做濾波幾次 
     blur_img = cv2.medianBlur(names['img99'],3)
     return blur_img
 def Sobel_gradient( gray_img, direction = 1 ):
@@ -74,10 +34,12 @@ def Sobel_gradient( gray_img, direction = 1 ):
 		grad_x = cv2.filter2D( gray_img, cv2.CV_32F, sobel_x )
 		gx = abs( grad_x )
 		g = np.uint8( np.clip( gx, 0, 255 ) )
+        #梯度 x
 	elif direction == 2:
 		grad_y = cv2.filter2D( gray_img, cv2.CV_32F, sobel_y )
 		gy = abs( grad_y )
 		g = np.uint8( np.clip( gy, 0, 255 ) )
+        #梯度y
 	else:
 		grad_x = cv2.filter2D( gray_img, cv2.CV_32F, sobel_x )
 		grad_y = cv2.filter2D( gray_img, cv2.CV_32F, sobel_y )
@@ -86,13 +48,16 @@ def Sobel_gradient( gray_img, direction = 1 ):
 	return g
 def devided(gray_img):#二值化
     nr,nc = gray_img.shape[:2]
+    #取出高與寬
     black_white_img = gray_img.copy()
+    #複製原圖
     for i in range(0,nr):
         for j in range(0,nc):
             if(gray_img[i][j] >= 128): 
                 black_white_img[i][j] = 255
             elif(gray_img[i][j] < 128):
                 black_white_img[i][j] = 0
+    #分化兩值
     return black_white_img
 def hole_filling(img):
     kernal = np.ones((3,3),np.uint8)#創kernal
@@ -100,58 +65,103 @@ def hole_filling(img):
     return g
 def getHProjection(img):
     hProjection = np.zeros(img.shape,np.uint8)
-    #图像高与宽
-    (h,w)=img.shape 
+    #圖像高與寬
+    (h,w)=img.shape
+    #取出高與寬 
     arr = []
-    #长度与图像高度一致的数组
     h_ = [0]*h
-    #循环统计每一行白色像素的个数
     for y in range(h):
         for x in range(w):
             if img[y,x] == 255:
                 h_[y]+=1
-    #绘制水平投影图像
+    #循環統計每一行像素的白色點個數
     for y in range(h):
         for x in range(h_[y]):
             hProjection[y,x] = 255
+    #繪製水平投影圖像
     for i in range(h):
-        if h_[i] < 10:
+        if h_[i] < 10:#當一行中的白點個數少於10時代表可能是斷層
             arr.append(i)
     print(arr)
     for i in range(len(arr)-1):
-        if (arr[i+1]-arr[i])>=300:
+        if (arr[i+1]-arr[i])>=300:#因車牌字符差不多高為300pixel
             point1 = (0,arr[i])
             point2 = (650,arr[i+1])
             img = img[point1[1]:point2[1] , point1[0]:point2[0]]
     cv2.imshow('hProjection',hProjection)
+    #show 橫圖
     return img
 def getVProjection(image):
     names = locals()
     arr = []
     vProjection = np.zeros(image.shape,np.uint8)
-    #图像高与宽
+    #圖像高與寬
     (h,w) = image.shape
-    #长度与图像宽度一致的数组
+    #取出長與寬
     w_ = [0]*w
-    #循环统计每一列白色像素的个数
     for x in range(w):
         for y in range(h):
             if image[y,x] == 255:
                 w_[x]+=1
-    #绘制垂直平投影图像
+    #循環統計每一列的白色個數
     for x in range(w):
         for y in range(h-w_[x],h):
             vProjection[y,x] = 255
+    #繪製垂直投影圖像
     for i in range(w):
-        if w_[i] == 0 or i == w-1:
-            arr.append(i)    
+        if w_[i] ==0 or i == w-1:
+            arr.append(i)
+        #去切分全部黑色的部份    
     print(arr)
     for i in range(len(arr)-1):
-        if (arr[i+1]-arr[i])>=50:
+        if (arr[i+1]-arr[i])>=50:#當兩個黑色間距>=50代表隔了一個字母
             point1 = (arr[i],0)
             point2 = (arr[i+1],500)
             names['x%s' %i] = image[point1[1]:point2[1] , point1[0]:point2[0]]
+            #得出每一塊字符
             cv2.imshow("img"+ str(i),names['x' + str(i)])
     cv2.imshow('vProjection',vProjection)
+    #show v圖
     return image
+def main():
+    input_img = cv2.imread("images/car.jpg",-1)
+    #讀入汽車圖片
+    trans_img = transform(input_img)
+    #取出車牌的部份圖形
+    gray_img = cvt_gray(trans_img)
+    #轉換為灰階去二值化
+    grad_img = Sobel_gradient(gray_img,3)
+    #使用梯度讓車牌輪廓更明顯
+    black_white_img = devided(grad_img)
+    #二值化
+    blur_img = blur(black_white_img)
+    #濾波器濾掉雜質
+    nohole_img = hole_filling(blur_img)
+    #去除白色破洞
+    canny_img = cv2.Canny(nohole_img,30,100)
+    #找出線條
+    himage = getHProjection(canny_img)
+    #水平切切出文字
+    yimage = getVProjection(himage)
+    #垂直切分割文字
+    cv2.imshow("car_origin",input_img)
+    #show 原始圖 
+    cv2.imshow("after_trans",trans_img)
+    #show 翻轉圖
+    cv2.imshow("gray_pic",gray_img)
+    #show 灰度圖
+    cv2.imshow("grad_pic", grad_img)
+    # show 梯度圖
+    cv2.imshow("blur_pic",blur_img)
+    # show 濾波圖
+    cv2.imshow("nohole_pic",nohole_img)
+    #show補漏洞圖
+    cv2.imshow("black_white_pic",black_white_img)
+    #show二值化後的圖
+    cv2.imshow("canny_pic",canny_img)
+    #show線條圖
+    cv2.imshow("himage",himage)
+    #移除圖片雜質
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 main()
--- opencv/test1.py
+++ opencv/test1.py
@@ -1,10 +1,64 @@
 import cv2
-import matplotlib.pyplot as plt
-img = cv2.imread("images/pic01.jpg" , -1)
-cv2.namedWindow("ex1",0)
-cv2.resizeWindow("ex1",550,550)
-cv2.imshow("ex1", img)
-#!!!!!! 不加沒辦法運行
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-#!!!!!! 
+import numpy as np
+def lpr(filename):
+    img = cv2.imread(filename)
+    # 預處理，包括灰度處理，高斯濾波平滑處理，Sobel提取邊界，影象二值化
+    # 對於高斯濾波函式的引數設定，第四個引數設為零，表示不計算y方向的梯度，原因是車牌上的數字在豎方向較長，重點在於得到豎方向的邊界
+    # 對於二值化函式的引數設定，第二個引數設為127，是二值化的閾值，是一個經驗值
+    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    GaussianBlur_img = cv2.GaussianBlur(gray_img, (3, 3), 0)
+    Sobel_img = cv2.Sobel(GaussianBlur_img, -1, 1, 0, ksize=3)
+    ret, binary_img = cv2.threshold(Sobel_img, 127, 255, cv2.THRESH_BINARY)
+
+    # 形態學運算
+    kernel = np.ones((5, 15), np.uint8)
+    # 先閉運算將車牌數字部分連線，再開運算將不是塊狀的或是較小的部分去掉
+    close_img = cv2.morphologyEx(binary_img, cv2.MORPH_CLOSE, kernel)
+    open_img = cv2.morphologyEx(close_img, cv2.MORPH_OPEN, kernel)
+    # kernel2 = np.ones((10, 10), np.uint8)
+    # open_img2 = cv2.morphologyEx(open_img, cv2.MORPH_OPEN, kernel2)
+    # 由於部分影象得到的輪廓邊緣不整齊，因此再進行一次膨脹操作
+    element = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    dilation_img = cv2.dilate(open_img, element, iterations=3)
+
+    # 獲取輪廓
+    contours, hierarchy = cv2.findContours(dilation_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # 測試邊框識別結果
+    # cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
+    # cv2.imshow("lpr", img)
+    # cv2.waitKey(0)
+
+    # 將輪廓規整為長方形
+    rectangles = []
+    for c in contours:
+        x = []
+        y = []
+        for point in c:
+            y.append(point[0][0])
+            x.append(point[0][1])
+        r = [min(y), min(x), max(y), max(x)]
+        rectangles.append(r)
+
+    # 用顏色識別出車牌區域
+    # 需要注意的是這裡設定顏色識別下限low時，可根據識別結果自行調整
+    dist_r = []
+    max_mean = 0
+    for r in rectangles:
+        block = img[r[1]:r[3], r[0]:r[2]]
+        hsv = cv2.cvtColor(block, cv2.COLOR_BGR2HSV)
+        low = np.array([100, 60, 60])
+        up = np.array([140, 255, 255])
+        result = cv2.inRange(hsv, low, up)
+        # 用計算均值的方式找藍色最多的區塊
+        mean = cv2.mean(result)
+        if mean[0] > max_mean:
+            max_mean = mean[0]
+            dist_r = r
+
+    # 畫出識別結果，由於之前多做了一次膨脹操作，導致矩形框稍大了一些，因此這裡對於框架+3-3可以使框架更貼合車牌
+    cv2.rectangle(img, (dist_r[0]+3, dist_r[1]), (dist_r[2]-3, dist_r[3]), (0, 255, 0), 2)
+    cv2.imshow("lpr", img)
+    cv2.waitKey(0)
+
+# 主程式
+lpr("images/car.jpg")
